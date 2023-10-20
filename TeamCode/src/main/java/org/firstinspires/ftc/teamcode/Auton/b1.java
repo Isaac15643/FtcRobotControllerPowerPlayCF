d29bb077d1a3f888287cfcdd0d4bf6b4fb9fc4fe
vicksburgcontrolfreaks <90581349+vicksburgcontrolfreaks@users.noreply.github.com>
2023-10-20 17:07:15 -0400

    Added b1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/b1.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.Auton;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.MecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Autonomous
+
+public class b1 extends LinearOpMode {
+    @Override
+
+
+    public void runOpMode() throws InterruptedException {
+        MecanumDrive drivetrain = new MecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(-36, 60, Math.toRadians(270)); //tell the robot where it starts
+
+
+
+        TrajectorySequence tragic = drivetrain.trajectorySequenceBuilder(startPose)
+                .lineTo(new Vector2d(-36,36))
+                .waitSeconds(2.5)
+                .lineToSplineHeading(new Pose2d(-36,45,Math.toRadians(0)))
+//                                .splineTo(new Vector2d(12,58),Math.toRadians(180))
+                .splineToLinearHeading(new Pose2d(18,58),Math.toRadians(0))
+                .lineToLinearHeading(new Pose2d(50,36,Math.toRadians(180)))
+                .splineTo(new Vector2d(38,7),Math.toRadians(180))
+                .splineTo(new Vector2d(-62,12),Math.toRadians(180))
+
+                        .build();
+
+        waitForStart();
+
+        if (!isStopRequested()) {
+            drivetrain.followTrajectorySequence(tragic);
+
+
+        }
+    }
+}
