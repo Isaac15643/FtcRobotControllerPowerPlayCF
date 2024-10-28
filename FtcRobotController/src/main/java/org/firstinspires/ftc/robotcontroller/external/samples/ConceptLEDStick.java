70ec25d503684db0ed27babdd25bfb1720dbb74b
vicksburgcontrolfreaks <90581349+vicksburgcontrolfreaks@users.noreply.github.com>
2024-10-28 18:39:33 -0400

    Revert "10/14/24"
    This reverts commit 252a5ac6af2b53cc9352f00121e48c0e1a172bf4.

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptLEDStick.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptLEDStick.java
@@ -32,92 +32,92 @@ package org.firstinspires.ftc.robotcontroller.external.samples;
         THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-//import android.graphics.Color;
-//
-//import com.qualcomm.hardware.sparkfun.SparkFunLEDStick;
-//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import com.qualcomm.robotcore.util.Range;
+import android.graphics.Color;
 
-///*
-// * This OpMode illustrates how to use the SparkFun QWIIC LED Strip
-// *
-// * This is a simple way to add a strip of 10 LEDs to your robot where you can set the color of each
-// * LED or the whole strip.   This allows for driver feedback or even just fun ways to show your team
-// * colors.
-// *
-// * Why?
-// * Because more LEDs == more fun!!
-// *
-// * This OpMode assumes that the QWIIC LED Stick is attached to an I2C interface named "back_leds" in the robot configuration.
-// *
-// * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
-// * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
-// *
-// * You can buy this product here:  https://www.sparkfun.com/products/18354
-// * Don't forget to also buy this to make it easy to connect to your Control or Expansion Hub:
-// * https://www.sparkfun.com/products/25596
-// */
-//@TeleOp(name = "Concept: LED Stick", group = "Concept")
-//@Disabled
-//public class ConceptLEDStick extends OpMode {
-//    private boolean wasUp;
-//    private boolean wasDown;
-//    private int brightness = 5;  // this needs to be between 0 and 31
-//    private final static double END_GAME_TIME = 120 - 30;
-//
-//    private SparkFunLEDStick ledStick;
-//
-//    @Override
-//    public void init() {
-//        ledStick = hardwareMap.get(SparkFunLEDStick.class, "back_leds");
-//        ledStick.setBrightness(brightness);
-//        ledStick.setColor(Color.GREEN);
-//    }
-//
-//    @Override
-//    public void start() {
-//        resetRuntime();
-//    }
-//
-//    @Override
-//    public void loop() {
-//        telemetry.addLine("Hold the A button to turn blue");
-//        telemetry.addLine("Hold the B button to turn red");
-//        telemetry.addLine("Hold the left bumper to turn off");
-//        telemetry.addLine("Use DPAD Up/Down to change brightness");
-//
-//        if (getRuntime() > END_GAME_TIME) {
-//            int[] ledColors = {Color.RED, Color.YELLOW, Color.RED, Color.YELLOW, Color.RED,
-//                    Color.YELLOW, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW};
-//            ledStick.setColors(ledColors);
-//        } else if (gamepad1.a) {
-//            ledStick.setColor(Color.BLUE);
-//        } else if (gamepad1.b) {
-//            ledStick.setColor(Color.RED);
-//        } else if (gamepad1.left_bumper) {
-//            ledStick.turnAllOff();
-//        } else {
-//            ledStick.setColor(Color.GREEN);
-//        }
-//
-//        /*
-//         * Use DPAD up and down to change brightness
-//         */
-//        int newBrightness = brightness;
-//        if (gamepad1.dpad_up && !wasUp) {
-//            newBrightness = brightness + 1;
-//        } else if (gamepad1.dpad_down && !wasDown) {
-//            newBrightness = brightness - 1;
-//        }
-//        if (newBrightness != brightness) {
-//            brightness = Range.clip(newBrightness, 0, 31);
-//            ledStick.setBrightness(brightness);
-//        }
-//        telemetry.addData("Brightness", brightness);
-//
-//        wasDown = gamepad1.dpad_down;
-//        wasUp = gamepad1.dpad_up;
-//    }
-//}
+import com.qualcomm.hardware.sparkfun.SparkFunLEDStick;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+/*
+ * This OpMode illustrates how to use the SparkFun QWIIC LED Strip
+ *
+ * This is a simple way to add a strip of 10 LEDs to your robot where you can set the color of each
+ * LED or the whole strip.   This allows for driver feedback or even just fun ways to show your team
+ * colors.
+ *
+ * Why?
+ * Because more LEDs == more fun!!
+ *
+ * This OpMode assumes that the QWIIC LED Stick is attached to an I2C interface named "back_leds" in the robot configuration.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ *
+ * You can buy this product here:  https://www.sparkfun.com/products/18354
+ * Don't forget to also buy this to make it easy to connect to your Control or Expansion Hub:
+ * https://www.sparkfun.com/products/25596
+ */
+@TeleOp(name = "Concept: LED Stick", group = "Concept")
+@Disabled
+public class ConceptLEDStick extends OpMode {
+    private boolean wasUp;
+    private boolean wasDown;
+    private int brightness = 5;  // this needs to be between 0 and 31
+    private final static double END_GAME_TIME = 120 - 30;
+
+    private SparkFunLEDStick ledStick;
+
+    @Override
+    public void init() {
+        ledStick = hardwareMap.get(SparkFunLEDStick.class, "back_leds");
+        ledStick.setBrightness(brightness);
+        ledStick.setColor(Color.GREEN);
+    }
+
+    @Override
+    public void start() {
+        resetRuntime();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addLine("Hold the A button to turn blue");
+        telemetry.addLine("Hold the B button to turn red");
+        telemetry.addLine("Hold the left bumper to turn off");
+        telemetry.addLine("Use DPAD Up/Down to change brightness");
+        
+        if (getRuntime() > END_GAME_TIME) {
+            int[] ledColors = {Color.RED, Color.YELLOW, Color.RED, Color.YELLOW, Color.RED,
+                    Color.YELLOW, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW};
+            ledStick.setColors(ledColors);
+        } else if (gamepad1.a) {
+            ledStick.setColor(Color.BLUE);
+        } else if (gamepad1.b) {
+            ledStick.setColor(Color.RED);
+        } else if (gamepad1.left_bumper) {
+            ledStick.turnAllOff();
+        } else {
+            ledStick.setColor(Color.GREEN);
+        }
+
+        /*
+         * Use DPAD up and down to change brightness
+         */
+        int newBrightness = brightness;
+        if (gamepad1.dpad_up && !wasUp) {
+            newBrightness = brightness + 1;
+        } else if (gamepad1.dpad_down && !wasDown) {
+            newBrightness = brightness - 1;
+        }
+        if (newBrightness != brightness) {
+            brightness = Range.clip(newBrightness, 0, 31);
+            ledStick.setBrightness(brightness);
+        }
+        telemetry.addData("Brightness", brightness);
+
+        wasDown = gamepad1.dpad_down;
+        wasUp = gamepad1.dpad_up;
+    }
+}
